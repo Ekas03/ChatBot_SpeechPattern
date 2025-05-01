16033e1e3efde8c87e0e1dfa3f8c432bac9d6cf8
Egor Skorodumov <edskorodumov_1@edu.hse.ru>
2025-05-01 19:17:49 +0300

    Revert "update proj"
    This reverts commit e8d32924754998cc03056849004e87ee0aec7301.

--- app/bot.py
+++ app/bot.py
@@ -12,8 +12,6 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-login = 1
-
 Base = declarative_base()
 engine = create_async_engine(DB_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
